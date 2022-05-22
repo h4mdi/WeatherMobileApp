4a50093aeeb6ae4083f0535a9508d5d56d608a28
h4mdi <hamdi.bourounia@sesame.com.tn>
2022-05-22 01:51:28 +0100

    last commit prod

---
+++ app/src/main/java/com/example/weatherinfo/MainActivity.java
@@ -0,0 +1,239 @@
+package com.example.weatherinfo;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener{
+    LocationManager locationManager;
+    String CITY;
+    String API = "78f720462b09545abee531d48aa04f4e";
+    ImageView search;
+    EditText etCity;
+    TextView city,country,time,temp,forecast,humidity,min_temp,max_temp,sunrises,sunsets,pressure,windSpeed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationEnabled();
+        getLocation();
+
+
+            etCity = findViewById(R.id.Your_city);
+            search = findViewById(R.id.search);
+
+            city = findViewById(R.id.city);
+            country = findViewById(R.id.country);
+            time = findViewById(R.id.time);
+            temp = findViewById(R.id.temp);
+            forecast =  findViewById(R.id.forecast);
+            humidity = findViewById(R.id.humidity);
+            min_temp =  findViewById(R.id.min_temp);
+            max_temp =  findViewById(R.id.max_temp);
+            sunrises =  findViewById(R.id.sunrises);
+            sunsets = findViewById(R.id.sunsets);
+            pressure = findViewById(R.id.pressure);
+            windSpeed = findViewById(R.id.wind_speed);
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        etCity.setText(addresses.get(0).getLocality());
+        CITY = etCity.getText().toString();
+        new weatherTask().execute();
+        search.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    CITY = etCity.getText().toString();
+                    new weatherTask().execute();
+                }
+            });
+        }
+
+
+    private void locationEnabled() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (!gps_enabled && !network_enabled) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enable GPS Service")
+                    .setMessage("We need your GPS location to show Near Places around you.")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new
+                            DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                }
+                            })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        }
+    }
+
+    void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5, (LocationListener) this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            etCity.setText(addresses.get(0).getLocality());
+            CITY = etCity.getText().toString();
+            new weatherTask().execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    class weatherTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+
+
+
+                // CALL VALUE IN API :
+                String city_name = jsonObj.getString("name");
+                String countryname = sys.getString("country");
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Last Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temperature = main.getString("temp");
+                String cast = weather.getString("description");
+                String humi_dity = main.getString("humidity");
+                String temp_min = main.getString("temp_min");
+                String temp_max = main.getString("temp_max");
+                String pre = main.getString("pressure");
+                String windspeed = wind.getString("speed");
+                Long rise = sys.getLong("sunrise");
+                String sunrise = new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(rise * 1000));
+                Long set = sys.getLong("sunset");
+                String sunset = new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(set * 1000));
+
+
+
+
+
+
+                // SET ALL VALUES IN TEXTBOX :
+                city.setText(city_name);
+                country.setText(countryname);
+                time.setText(updatedAtText);
+                temp.setText(temperature + "°C");
+                forecast.setText(cast);
+                humidity.setText(humi_dity);
+                min_temp.setText(temp_min);
+                max_temp.setText(temp_max);
+                sunrises.setText(sunrise);
+                sunsets.setText(sunset);
+                pressure.setText(pre);
+                windSpeed.setText(windspeed);
+
+            } catch (Exception e) {
+
+                Toast.makeText(MainActivity.this, "Error:" + e.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
